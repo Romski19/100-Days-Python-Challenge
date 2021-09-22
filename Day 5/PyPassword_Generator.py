a01fffee3aad9256d29ccd9e4921bcf8e1328916
Romski19 <42634501+Romski19@users.noreply.github.com>
2021-09-22 09:37:57 +0000

    added the last code exercise for Day 5 - Pypassword generator - For Loop, random

--- Day 5/PyPassword_Generator.py
+++ Day 5/PyPassword_Generator.py
@@ -12,6 +12,9 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
 password = ""
 
 for char in range(1 , nr_letters + 1):
@@ -27,11 +30,6 @@ password_choice = ''.join(random.sample(password, len(password)))
 
 print(str(password_choice))
 
-  # print(chosen_letter, chosen_symbols, chosen_number, end="")
 
 
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
