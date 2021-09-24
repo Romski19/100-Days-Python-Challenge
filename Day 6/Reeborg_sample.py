5800e3de504e6134294aa4121702517688869f30
Romski19 <42634501+Romski19@users.noreply.github.com>
2021-09-24 02:39:03 +0000

    Adding Day 6 - Functions - sample code for reeborg.ca

---
+++ Day 6/Reeborg_sample.py
@@ -0,0 +1,25 @@
+#open this -> https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+
+#turning_around
+def turn_around():
+    turn_left()
+    turn_left()
+  
+# sample of turning right    
+def turn_right():
+    turn_around()
+    turn_left()
+    
+move()
+move()
+move()
+turn_left()
+move()
+move()
+turn_right()
+move()
+move()
+
+
+# move_box
