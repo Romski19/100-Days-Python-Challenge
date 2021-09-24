863546c5c6e16bbe8e3d7afe78d8f1757b800f6c
Romski19 <42634501+Romski19@users.noreply.github.com>
2021-09-24 03:32:58 +0000

    Hurdles - While loop

--- Day 6/HurdleChallenge_WhileLoop.py
+++ Day 6/HurdleChallenge_WhileLoop.py
@@ -1,3 +1,5 @@
+#while loops https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
 def turn_right():
     turn_left()
     turn_left()
