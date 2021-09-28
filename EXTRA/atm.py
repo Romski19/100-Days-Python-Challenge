b1f56a99b63f05d25e28556aa4869efa2166bacd
Romski19 <42634501+Romski19@users.noreply.github.com>
2021-09-28 15:18:39 +0000

    added extra folder with ATM code challenge for practice

---
+++ EXTRA/atm.py
@@ -0,0 +1,55 @@
+
+balance = 0
+
+def company_name():
+  print("=== Welcome to Ramosa Group Bank - ATM ===")
+
+def deposit():
+  company_name()
+  print("How much do you want to deposit?")
+  deposited = int(input("Enter Amount: "))  
+  new_balance = balance + deposited
+  print(f"You're outstanding balance is: {new_balance}")
+  return(new_balance)
+
+def withdraw():
+  company_name()
+  print("How much do you want to withdraw?")
+  withdrawed = int(input("Enter Amount: "))
+  if withdrawed > balance:
+    print("The amount entered is greater than outstanding balance")
+    return(balance)
+  elif withdrawed < 0:
+    print("Invalid withdrawal amount!")
+    return(balance)
+  else:  
+    new_balance = balance - withdrawed
+    print(f"You're outstanding balance is: {new_balance}")
+    return(new_balance)
+
+def check_balance():
+  print(f"You're outstanding balance is: {balance}")
+
+should_continue = True
+
+while should_continue:
+  company_name()
+  print("=== Please select an option ===")
+  print("=== 1. Inquire Balance ===")
+  print("=== 2. Deposit         ===")
+  print("=== 3. Withdraw        ===")
+  chosen = int(input("Selection:"))
+  if chosen == 1:
+      check_balance()
+  elif chosen == 2:   
+      balance = deposit()
+  elif chosen == 3:
+      balance = withdraw()
+  else:
+      print("You have chosen a wrong option")
+  
+
+  result = input("Do you still wanna continue? Y/N: ").lower()
+  if result == "n":
+    should_continue = False
+    print("Goodbye")
--- main.py
+++ main.py
@@ -1,25 +1,2 @@
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
-# 🚨 Don't change the code above 👆
-
-#TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
-
-#TODO-2: Write your code below to add the grades to student_grades.👇
-for scores in student_scores:
-  if student_scores[scores] >= 91 and student_scores[scores] <= 100:
-    student_grades[scores] = "Outstanding"
-  elif student_scores[scores] >= 81 and student_scores[scores] <= 90:
-    student_grades[scores] = "Exceeds Expectations"
-  elif student_scores[scores] >= 71 and student_scores[scores] <= 80:
-    student_grades[scores] = "Acceptable"
-  else:
-    student_grades[scores] = "Fail"
-# 🚨 Don't change the code below 👇
-
-print(student_grades)
+for x in range(0, 101):
+  print(f"i love you {x}")
