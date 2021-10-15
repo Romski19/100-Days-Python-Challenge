60c0f3a412e40776bc9e1a8d334ecd7e8e26ca12
RomeoRamosa <romeoramosa@gmail.com>
2021-10-15 11:23:29 +0700

    learning attributes

--- Day - 16/main.py
+++ Day - 16/main.py
@@ -17,7 +17,6 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name",["Pikachu","Squirtle", "Charmander"])
 table.add_column("Type",["Electric", "Water", "Fire"])
-
+table.align = "l"
 print(table)
-
 
