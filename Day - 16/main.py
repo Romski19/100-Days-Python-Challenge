cdeb1b8d0cf0346887cbe994e09e084a11aac221
RomeoRamosa <romeoramosa@gmail.com>
2021-10-15 08:53:44 +0700

    Day 16 - Object Oriented Programming: Attributes, Methods, Class

---
+++ Day - 16/main.py
@@ -0,0 +1,12 @@
+# sample of class, method, attributes - OOP
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
