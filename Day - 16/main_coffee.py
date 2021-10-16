2fe0359f56ade168a7793fabd84b3339b5e24764
RomeoRamosa <romeoramosa@gmail.com>
2021-10-16 10:30:04 +0700

    added code challenge for day 16

--- Day - 16/main_coffee.py
+++ Day - 16/main_coffee.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money = MoneyMachine()
+available = CoffeeMaker()
+menu = Menu()
+
+to_continue = True
+
+while to_continue:
+    option = menu.get_items()
+    choice = input(f"What would you like to drink? {option}?: ").lower()
+    if choice == "off":
+        to_continue = False
+    elif choice == "report":
+        available.report()
+        money.report()
+    else:
+        drink = menu.find_drink(choice)
+        if available.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            available.make_coffee(drink)
