6940ae0f2e071a2609ae6c4b64b47a0e10e4170b
RomeoRamosa <romeoramosa@gmail.com>
2021-10-29 12:11:57 +0700

    day 20th and 21st - completed snake game - slicing, inheritance

--- Day - 20 and 21/main.py
+++ Day - 20 and 21/main.py
@@ -1,4 +1,6 @@
 from turtle import Turtle, Screen
+from food import Food
+from scoreboard import Scoreboard
 import time
 from snake import Snake
 
@@ -13,21 +15,36 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
-screen.onkey(snake.down,"Down")
+screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # detect collision with the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
+    #     detect collision with snake body
+    for segment in snake.snake_segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
---
+++ Day - 20 and 21/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.goto(0, 270)
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score:{self.score}", align="center", font=("Arial", 12, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center",font=("Arial", 20, "normal") )
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
