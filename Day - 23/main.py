a6a3ed7438eb5f2865345435c3e096a4f7e4b0aa
RomeoRamosa <romeoramosa@gmail.com>
2021-11-02 09:49:23 +0700

    day 23 - crossing turtle completed

---
+++ Day - 23/main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
+
+
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    cars.generate_car()
+    cars.move_cars()
+
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+#     successful crossing
+    if player.is_at_finsih_line():
+        player.go_to_start()
+        cars.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
---
+++ Day - 23/player.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.shape("turtle")
+        self.starting_point()
+
+    def starting_point(self):
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finsih_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
