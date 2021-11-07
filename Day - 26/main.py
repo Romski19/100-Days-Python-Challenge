177561edec0a0cad895b3cb3b26a4edb5cbeb908
RomeoRamosa <romeoramosa@gmail.com>
2021-11-07 15:43:13 +0700

    day 26 - list comprehension

---
+++ Day - 26/main.py
@@ -0,0 +1,9 @@
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# Square numbers
+# squared_numbers = [ n * n for n in numbers]
+# print(squared_numbers)
+
+# even numbers
+# even_numbers = [n for n in numbers if n % 2 == 0]
+# print(even_numbers)
