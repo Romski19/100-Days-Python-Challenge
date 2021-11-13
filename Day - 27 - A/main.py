945b08727dbee667f6a21a79e5ac4c27821957e6
RomeoRamosa <romeoramosa@gmail.com>
2021-11-13 09:39:03 +0700

    Day 27 - tkinter widgets

--- Day - 27 - A/main.py
+++ Day - 27 - A/main.py
@@ -1,14 +1,28 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("GUI by TKinter")
 window.minsize(width=600, height=600)
 
-#Label
+# Label
 
-my_label = tkinter.Label(text="Hello World", font=("Arial", 60, "bold"))
+my_label = Label(text="Hello World", font=("Arial", 60, "bold"))
 my_label.pack()
 
+# Input (textbox)
+input_text = Entry(width=50)
+input_text.pack()
+
+
+# function to change text from Hello world
+def button_click():
+    my_label["text"] = "New Text"
+    my_label.config(text=input_text.get())
+
+
+# button prop
+btn = Button(text="Click Me", command=button_click)
+btn.pack()
 
 # always at the end v
 window.mainloop()
