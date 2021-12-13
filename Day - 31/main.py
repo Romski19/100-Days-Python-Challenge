01c724e8cc08dd56838f1c63af3a3e24812816d9
RomeoRamosa <romeoramosa@gmail.com>
2021-12-13 11:57:41 +0700

    Day 31 - flash card - incomplete - added time delay for flip card

--- Day - 31/main.py
+++ Day - 31/main.py
@@ -4,12 +4,33 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+df = pd.read_csv("data/french_words.csv")
+result = df.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(result)
+    canvas.itemconfig(title_id, text="French", fill="black")
+    canvas.itemconfig(word_id, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_id, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(word_id, text=current_card["English"], fill="white")
+    canvas.itemconfig(title_id, text="English", fill="white")
+    canvas.itemconfig(card_id, image=card_back_img)
+
+
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-df = pd.read_csv("data/french_words.csv")
-result = df.to_dict(orient="records")
+# timer delay 3 seconds
+flip_timer = window.after(3000, flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
@@ -20,24 +41,6 @@ word_id = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-
-def next_card():
-    current_card = random.choice(result)
-    canvas.itemconfig(title_id, text=f"French", fill="black")
-    canvas.itemconfig(word_id, text=current_card["French"])
-    canvas.itemconfig(card_id, image=card_front_img)
-    delay()
-
-
-def flip_card():
-    canvas.itemconfig(title_id, text="English", fill="white")
-    canvas.itemconfig(card_id, image=card_back_img)
-
-
-def delay():
-    window.after(3000, flip_card)
-
-
 cross_image = PhotoImage(file="images/wrong.png")
 uk_btn = Button(image=cross_image, highlightthickness=0, command=next_card)
 uk_btn.grid(row=1, column=0)
@@ -47,6 +50,5 @@ known_btn = Button(image=check_image, highlightthickness=0, command=next_card)
 known_btn.grid(row=1, column=1)
 
 next_card()
-
 
 window.mainloop()
