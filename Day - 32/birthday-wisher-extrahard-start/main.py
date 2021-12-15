2dc596ceca0807ebed98722b16323043c2195e77
RomeoRamosa <romeoramosa@gmail.com>
2021-12-15 15:17:42 +0700

    Day 32 - SMTP - Birthday Wisher - Email automation

---
+++ Day - 32/birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,48 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+import datetime as dt
+import pandas as pd
+import random
+import smtplib
+
+now = dt.datetime.now()
+month = now.month
+today = now.day
+date = f"{month}-{today}"
+
+my_email = "testingtestromeo@gmail.com"
+password = "Rabanos@121916"
+
+df = pd.read_csv("birthdays.csv")
+
+for x in range(len(df.name)):
+    email_to_send = df.email[x]
+    csv_names = df.name[x]
+    csv_month = df.month[x]
+    csv_date = df.day[x]
+    birth_day = f"{csv_month}-{csv_date}"
+
+    if date == birth_day:
+        with open(f"letter_templates/letter_{random.randint(1, 3)}.txt", "r") as mail_template:
+            content = mail_template.read()
+            new_content = content.replace("[NAME]", f"{csv_names}")
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=email_to_send,
+                                msg=f"Subject: Have Birthday Blast!\n\n"
+                                    f"{new_content}")
+
+
+    else:
+        pass
--- Day - 32/main.py
+++ Day - 32/main.py
@@ -1,21 +1,29 @@
-# import smtplib
-#
-# my_email = "testingtestromeo@gmail.com"
-# password = "Rabanos@121916"
-#
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs="romeoramosa@gmail.com",
-#                         msg="Subject:Hello Test\n\nTesting Email")
-
+import random
+import smtplib
 import datetime as dt
+
+my_email = "testingtestromeo@gmail.com"
+password = "Rabanos@121916"
 
 now = dt.datetime.now()
 year = now.year
 month = now.month
 day_of_the_week = now.weekday()
 
-date_of_birth = dt.datetime(year=1990, month=10, day=24)
+if day_of_the_week == 2:
+    with open("quotes.txt", "r") as qt:
+        all_qoutes = qt.readlines()
+
+    send_qoute = random.choice(all_qoutes)
+
+
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="roxan.b.rabanos@gmail.com",
+                            msg=f"Subject:Wednesday Quote\n\n{send_qoute}")
+else:
+    pass
+
