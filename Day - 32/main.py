2ae692a80c384d0c044096582821a635a873079d
RomeoRamosa <romeoramosa@gmail.com>
2021-12-15 11:14:34 +0700

    Day 32 - SMTP - adding datetime class

--- Day - 32/main.py
+++ Day - 32/main.py
@@ -1,12 +1,21 @@
-import smtplib
+# import smtplib
+#
+# my_email = "testingtestromeo@gmail.com"
+# password = "Rabanos@121916"
+#
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="romeoramosa@gmail.com",
+#                         msg="Subject:Hello Test\n\nTesting Email")
 
-my_email = "testingtestromeo@gmail.com"
-password = "Rabanos@121916"
+import datetime as dt
 
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_the_week = now.weekday()
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="romeoramosa@gmail.com",
-                        msg="Subject:Hello Test\n\nTesting Email")
+date_of_birth = dt.datetime(year=1990, month=10, day=24)
