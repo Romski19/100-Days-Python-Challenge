4cf513ce0119ea781002a3a2a0083e451be05d7d
RomeoRamosa <romeoramosa@gmail.com>
2021-12-15 10:58:34 +0700

    Day 32 - SMTP - Testing email

---
+++ Day - 32/main.py
@@ -0,0 +1,11 @@
+import smtplib
+
+my_email = "testingtestromeo@gmail.com"
+password = "Rabanos@121916"
+
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="romeoramosa@gmail.com", msg="Testing Email")
+connection.close()
