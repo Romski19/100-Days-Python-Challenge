cfc45c216bd1c53d22f2ff5bacff1eaf68497ec6
RomeoRamosa <romeoramosa@gmail.com>
2021-12-15 11:01:11 +0700

    Day 32 - SMTP - Testing email

--- Day - 32/main.py
+++ Day - 32/main.py
@@ -4,8 +4,7 @@ my_email = "testingtestromeo@gmail.com"
 password = "Rabanos@121916"
 
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="romeoramosa@gmail.com", msg="Testing Email")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="romeoramosa@gmail.com", msg="Subject:Hello Test\n\nTesting Email")
