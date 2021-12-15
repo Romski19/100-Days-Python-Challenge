9a8e4379b92c44d00bba4e5e513ad768c5358f3f
RomeoRamosa <romeoramosa@gmail.com>
2021-12-15 11:04:15 +0700

    Day 32 - SMTP - Testing email - refactor

--- Day - 32/main.py
+++ Day - 32/main.py
@@ -7,4 +7,6 @@ password = "Rabanos@121916"
 with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="romeoramosa@gmail.com", msg="Subject:Hello Test\n\nTesting Email")
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="romeoramosa@gmail.com",
+                        msg="Subject:Hello Test\n\nTesting Email")
