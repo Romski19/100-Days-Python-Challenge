65a240e7e0640541d689045b0b1865cfb6ebb47a
RomeoRamosa <romeoramosa@gmail.com>
2021-12-22 11:30:09 +0700

    Day 33 - API - Done Kanye qoute challenge

---
+++ Day - 33/main.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+longitude = response.json()["iss_position"]["longitude"]
+latitude = response.json()["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+
+print(iss_position)
