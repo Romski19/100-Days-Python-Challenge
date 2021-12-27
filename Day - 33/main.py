aa9cdea50299758be56aaf9b35dcfaf0697a21bc
RomeoRamosa <romeoramosa@gmail.com>
2021-12-27 15:59:30 +0700

    code challenge

--- Day - 33/main.py
+++ Day - 33/main.py
@@ -1,11 +1,24 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 13.756331
+MY_LONG = 100.501762
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# longitude = response.json()["iss_position"]["longitude"]
+# latitude = response.json()["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
 response.raise_for_status()
-
-longitude = response.json()["iss_position"]["longitude"]
-latitude = response.json()["iss_position"]["latitude"]
-
-iss_position = (longitude, latitude)
-
-print(iss_position)
+data = response.json()
+print(data)
--- main.py
+++ main.py
@@ -13,38 +13,52 @@
 
 # Saving Money in year 2022- Increment every 1 per day
 
+# days = [x for x in range(1, 366)]
+# length = len(days)
+#
+# y = 31
+# g = 30
+# x = 28
+#
+# j = days[0:y]
+# f = days[y:y + x]
+# m = days[f[x - 1]:f[x - 1] + y]
+# ap = days[m[g]:m[g] + g]
+# ma = days[ap[g - 1]:ap[g - 1] + y]
+# jn = days[ma[g]:ma[g] + g]
+# jl = days[jn[g - 1]:jn[g - 1] + y]
+# au = days[jl[g - 1]:jl[g - 1] + y]
+# s = days[au[g]:au[g] + g]
+# o = days[s[g - 1]:s[g - 1] + y]
+# n = days[o[g]:o[g] + g]
+# d = days[n[g - 1]:n[g - 1] + y]
+
+# print(f"total save: ",
+#       sum([sum(j), sum(f), sum(m), sum(ap), sum(ma), sum(jn), sum(jl), sum(au), sum(s), sum(o), sum(n), sum(d)]))
+#
+# print(f"month of January: ", (sum(j)))
+# print(f"month of February: ", (sum(f)))
+# print(f"month of March:", (sum(m)))
+# print(f"month of April: ", (sum(ap)))
+# print(f"month of May: ", (sum(ma)))
+# print(f"month of June: ", (sum(jn)))
+# print(f"month of July: ", (sum(jl)))
+# print(f"month of August: ", (sum(au)))
+# print(f"month of September: ", (sum(s)))
+# print(f"month of October: ", (sum(o)))
+# print(f"month of November: ", (sum(n)))
+# print(f"month of December: ", (sum(d)))
+
+
+from calendar import monthrange, month_name
+
+month_name = list(month_name[1:])
 days = [x for x in range(1, 366)]
-length = len(days)
 
-y = 31
-g = 30
-x = 28
+month_days = []
+for month in month_name:
+    month_days.append(monthrange(2021, list(month_name).index(month) + 1)[1])
 
-j = days[0:y]
-f = days[y:y + x]
-m = days[f[x - 1]:f[x - 1] + y]
-ap = days[m[g]:m[g] + g]
-ma = days[ap[g - 1]:ap[g - 1] + y]
-jn = days[ma[g]:ma[g] + g]
-jl = days[jn[g - 1]:jn[g - 1] + y]
-au = days[jl[g - 1]:jl[g - 1] + y]
-s = days[au[g]:au[g] + g]
-o = days[s[g - 1]:s[g - 1] + y]
-n = days[o[g]:o[g] + g]
-d = days[n[g - 1]:n[g - 1] + y]
+print("Total Saved:", sum([sum(days[sum(month_days[:i]):sum(month_days[:i+1])]) for i, j in enumerate(month_days)]))
+print([sum(days[sum(month_days[:i]):sum(month_days[:i+1])]) for i, j in enumerate(month_days)])
 
-print(f"total save: ",
-      sum([sum(j), sum(f), sum(m), sum(ap), sum(ma), sum(jn), sum(jl), sum(au), sum(s), sum(o), sum(n), sum(d)]))
-
-print(f"month of January: ", (sum(j)))
-print(f"month of February: ", (sum(f)))
-print(f"month of March:", (sum(m)))
-print(f"month of April: ", (sum(ap)))
-print(f"month of May: ", (sum(ma)))
-print(f"month of June: ", (sum(jn)))
-print(f"month of July: ", (sum(jl)))
-print(f"month of August: ", (sum(au)))
-print(f"month of September: ", (sum(s)))
-print(f"month of October: ", (sum(o)))
-print(f"month of November: ", (sum(n)))
-print(f"month of December: ", (sum(d)))
