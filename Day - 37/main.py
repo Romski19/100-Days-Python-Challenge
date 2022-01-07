cb12d581872c48d27ac08f4c43d91e93c0ca4f75
RomeoRamosa <romeoramosa@gmail.com>
2022-01-07 17:04:09 +0700

    Day 37 - Habbit Tracking - Undone - Post Add Graphic (Pixela)

--- Day - 37/main.py
+++ Day - 37/main.py
@@ -1,7 +1,9 @@
 import requests
+from datetime import datetime
 
 USERNAME = "romski07"
 TOKEN = "da52das64adw6dadaw"
+GRAPH_ID = "romeyaw19"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -18,16 +20,31 @@ parameters = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graphic_config = {
-    "id": "romeyaw19",
+    "id": GRAPH_ID,
     "name": "Diet Graph",
     "unit": "calorie",
     "type": "int",
     "color": "sora",
 }
 
+
+today = datetime(year=2022, month=1, day=5)
+
+post_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+input_param = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "300",
+    # "optionalData": "Limit"
+}
+
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
 
-response = requests.post(url=graph_endpoint, json=graphic_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graphic_config, headers=headers)
+# print(response.text)
+
+response = requests.post(url=post_endpoint, json=input_param, headers=headers)
 print(response.text)
+# print(today)
