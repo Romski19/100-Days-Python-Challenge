31e5084ec475cd7474563162bf946436ce99a291
RomeoRamosa <romeoramosa@gmail.com>
2022-01-17 15:17:38 +0700

    sample for web scrapping

--- Day - 45/main.py
+++ Day - 45/main.py
@@ -1,6 +1,21 @@
+from os import link
 from bs4 import BeautifulSoup
+# import lxml
 
 with open("Day - 45/website.html","r", encoding="UTF-8") as site:
     contents = site.read()
 
-print(contents)
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.prettify())
+# print(soup.a)
+
+# Find All Sample
+
+# print(soup.find_all('p'))
+# for ps in soup.find_all('p'):
+#     print(ps.get_text())
+
+
+for ps in soup.find_all('a'):
+    print(ps.get_text())
+    print(ps.get('href'))
