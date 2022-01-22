3c720ddbd028b58b8983348673af6bc46a5880d6
RomeoRamosa <romeoramosa@gmail.com>
2022-01-22 19:57:27 +0700

    basic use selenium

--- Day - 48/main.py
+++ Day - 48/main.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-chrome_driver_path = "C:\Users\Roxan\chromedriver"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+chrome_driver_path = "C:\development\chromedriver.exe"
+service = ChromeService(executable_path=chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.amazon.com')
+
+# driver.close()
+driver.quit()
