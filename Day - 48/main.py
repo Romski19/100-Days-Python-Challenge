67b70dce5a11e60c648df62cb1a581da2aa254f9
RomeoRamosa <romeoramosa@gmail.com>
2022-01-22 20:35:56 +0700

    challenge done

--- Day - 48/main.py
+++ Day - 48/main.py
@@ -5,7 +5,11 @@ chrome_driver_path = "C:\development\chromedriver.exe"
 service = ChromeService(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
-driver.get('https://www.amazon.com')
+driver.get('https://www.gmanetwork.com/news/')
+just_in = driver.find_elements_by_class_name("just-in-story")
+
+for j_news in just_in:
+    print(j_news.text)
 
 # driver.close()
 driver.quit()
