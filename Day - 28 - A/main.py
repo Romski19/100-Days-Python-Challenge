34f58a6640d62bec6031fc82dc9c590ee38fcb9a
RomeoRamosa <romeoramosa@gmail.com>
2022-01-24 20:39:23 +0700

    just correcting some stuffs

--- Day - 28 - A/main.py
+++ Day - 28 - A/main.py
@@ -77,7 +77,7 @@ title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YEL
 title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
+tomato_img = PhotoImage(file="Day - 28 - A/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
