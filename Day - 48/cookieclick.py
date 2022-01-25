5b8d5cbd4432f06d3e0558b0bef8e9e1a70aca06
RomeoRamosa <romeoramosa@gmail.com>
2022-01-25 19:56:41 +0700

    Day 48 Initial Resolution

--- Day - 48/cookieclick.py
+++ Day - 48/cookieclick.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import time
 import datetime
 from selenium import webdriver
@@ -7,30 +8,9 @@ from selenium.webdriver.common.by import By
 chrome_driver_path = "C:\development\chromedriver.exe"
 service = ChromeService(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service)
-
+driver.implicitly_wait(60)
 driver.get('https://orteil.dashnet.org/cookieclicker/')
 btn = driver.find_element(By.XPATH, "//*[@id='bigCookie']")
-# timeout = 5
-# end = 300   # 5 minutes from now
-
-
-
-# def game():
-#     interval_time = time.time()
-#     while time.time() < interval_time + timeout:
-#         test = 0
-#         btn.click()
-#         if test == timeout:
-#             break
-#         test -= 1
-
-# main_time = time.time()
-# while time.time() < main_time + end:
-#     test = 0
-#     game()
-#     if test == end:
-#         break
-#     test -= 1
 
 
 timeout = 5
@@ -40,13 +20,50 @@ time_int = 8
 current_time = datetime.datetime.now()
 time_change = datetime.timedelta(seconds=count_down)
 
+
+def check_upgrade(cookie_count):
+
+    if cookie_count >= 330000000:
+        btn8 = driver.find_element(By.XPATH,'//*[@id="productPrice7"]')
+        btn8.click()
+        print(cookie_count)
+    elif cookie_count >= 20000000:
+        btn7 = driver.find_element(By.XPATH, '//*[@id="product6"]/div[3]')
+        btn7.click()
+        print(cookie_count)
+    elif cookie_count >= 1400000:
+        btn6 = driver.find_element(By.XPATH,'//*[@id="productPrice5"]')
+        btn6.click()
+        print(cookie_count)
+    elif cookie_count >= 130000:
+        btn5 = driver.find_element(By.XPATH,'//*[@id="product4"]/div[3]')
+        btn5.click()
+        print(cookie_count)
+    elif cookie_count >= 64204:
+        btn4 = driver.find_element(By.XPATH,'//*[@id="product3"]/div[3]')
+        btn4.click()
+        print(cookie_count)
+    elif cookie_count >= 1673:
+        btn3 = driver.find_element(By.XPATH,'//*[@id="product2"]/div[3]')
+        btn3.click()
+        print(cookie_count)
+    elif cookie_count >= 153:
+        btn2 = driver.find_element(By.XPATH, '//*[@id="product1"]/div[3]')
+        btn2.click()
+        print(cookie_count)
+    elif cookie_count >= 40:
+        btn1 = driver.find_element(By.XPATH, '//*[@id="product0"]/div[3]')
+        btn1.click()
+        print(cookie_count)
+
+    else:
+        pass
+
+
 def game():
-        # x = 0
         interval_time = time.time()
         while time.time() < interval_time + time_int:
             test = 0
-            # x += 1
-            # print(x) 
             btn.click()
             if test == time_int:
                 break
@@ -56,9 +73,16 @@ while current_time != time_change:
     game()
     time.sleep(timeout)
     # check the score
-    driver.find_element(By.XPATH, '//*[@id="product0"]').click()
+    cookie_number = driver.find_element(By.CSS_SELECTOR,'#cookies').text
+    str_cookie = cookie_number.split()[0]
+    if ',' in str_cookie:
+        cookies = int(str_cookie.replace(',',''))
+    else:
+        cookies = int(str_cookie)
+    check_upgrade(cookies)
+    
+
     
 
 driver.quit()
 
-# do something
