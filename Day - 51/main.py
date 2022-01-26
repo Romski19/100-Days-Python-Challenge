6c95c39768c72800782c68f83bad4a4026ec2469
RomeoRamosa <romeoramosa@gmail.com>
2022-01-26 14:36:48 +0700

    initial

--- Day - 51/main.py
+++ Day - 51/main.py
@@ -11,5 +11,10 @@ service = ChromeService(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 driver.implicitly_wait(60)
 
-driver.get('https://twitter.com/i/flow/login')
+driver.get('https://th.speedtest.net/')
+btn = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+btn.click()
+
+# Twitter Driver
+# tw = driver.get('https://twitter.com/i/flow/login')
 
