b19fe12f4714c32bee5a504923035e68e8847958
RomeoRamosa <romeoramosa@gmail.com>
2022-02-23 16:37:24 +0700

    location tracker

--- IPMap.py
+++ IPMap.py
@@ -7,5 +7,7 @@ my_loc = g.latlng
 
 my_map = folium.Map(location=my_loc,
                     zoom_start=20)
+folium.CircleMarker(location=my_loc,
+                    radius=50, popup="AirportLink").add_to(my_map)
 
 my_map.save("map.html")
